091971c148f92e8f72357ae8e7b34846e9b9ce82
Bmorrell23 <blknm4598@gmail.com>
2019-12-01 14:07:45 -0600

    Change macAddress

--- www/js/index.js
+++ www/js/index.js
@@ -17,7 +17,7 @@
  * under the License.
  */
  var app = {
-     macAddress: "A4:CF:12:75:C8:82",  // get your mac address from bluetoothSerial.list
+     macAddress: "A4:CF:12:75:A6:56",  // get your mac address from bluetoothSerial.list
      chars: "",
 
  /*
